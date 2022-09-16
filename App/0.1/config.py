d9b1df15701a034890979b70bf598f5b53955f94
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-09-16 16:39:10 +0300

    Update config.py

--- App/0.1/config.py
+++ App/0.1/config.py
@@ -1,6 +1,6 @@
 class Config:
     def __init__(self):
-        self.token = "5740128555:AAFvsTfZBh-HNQtNBoIKeLy79PFToxt8ohU"
-        self.main_user_id = 1137363239
+        self.token = ":-"
+        self.main_user_id = 
 
-        self.vk_token = '2c5b8ba12c5b8ba12c5b8ba1332f4bffce22c5b2c5b8ba14f5cb3d0ccead3578c9270a6'
+        self.vk_token = ''
